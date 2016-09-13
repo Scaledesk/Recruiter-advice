eeaff4e83aa3364e411ec5d60f36f902d4a9459c
sanchit <sanchit@scorecard.dreamhost.com>
2016-09-13 01:12:50 -0700

    aswfwef

--- admin/connection.php
+++ admin/connection.php
@@ -1,5 +1,5 @@
 <?php
 error_reporting (1);
-$con=mysql_connect("localhost","root","abc") or die(mysql_error());
-mysql_select_db("Recruiters_advise",$con);
+$con=mysql_connect("mysql.recruiter.scaledesk.com","recruiter101","qazplmq1w2e3r4") or die(mysql_error());
+mysql_select_db("recruiters_advise",$con);
 ?>
